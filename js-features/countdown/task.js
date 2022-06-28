c0f80bf2e749e5e0cd3277140198dfe7e7f08110
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-06-28 23:44:24 +0300

    js-features: countdown completed in simple version

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+let timer = document.getElementById('timer');
+let current;
+function countDown(timer) {
+  current = timer.textContent;
+  current -= 1;
+  timer.textContent = current;
+  if (current === 0) {
+    alert('Вы победили в конкурсе');
+    clearInterval(intervalId);
+  }
+};
+let intervalId = window.setInterval(countDown, 100, timer);
