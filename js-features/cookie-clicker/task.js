ac224c9f120e54f5e75486952f75845e3d022fb3
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-06-29 21:45:14 +0300

    js-features: clicker is done. Easy way

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,11 +2,6 @@ const counter = document.getElementById('clicker__counter');
 const cookie = document.querySelector('#cookie');
 let currentValue = Number(counter.textContent);
 
-//
-console.log(`counter = ${counter}`);
-console.log(`cookie = ${cookie}`);
-//
-
 cookie.onclick = function () {
   this.width = 250;
   currentValue += 1;
