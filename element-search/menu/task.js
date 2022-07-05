9b948237e6b2055ec1ff7f2460743bee100b98d3
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-07-05 15:00:46 +0300

    Element-search: second task is solved in dificulty variant 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,18 @@
 const menuLinks = document.querySelectorAll('a.menu__link');
+const subMenus = document.querySelectorAll('ul.menu_sub');
 
 menuLinks.forEach(item => {
   item.addEventListener('click', e => {
+    subMenus.forEach(subMenu => {
+      if (subMenu.classList.contains('menu_active')) {
+        subMenu.classList.remove('menu_active');
+      };
+    });
+
     if (item.parentElement.querySelector('ul')) {
       e.preventDefault();
     };
+    
     item.parentElement.querySelector('ul').classList.toggle('menu_active');    
   });
 });
