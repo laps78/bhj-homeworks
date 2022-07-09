aa8fef801db5fda894e86e052324d2a7bafe9c10
laps78 <9254384@mail.ru>
2022-07-09 21:02:27 +0300

    Element-search: third task reclamations are  complete

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,35 +1,43 @@
 const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
 const sliderArrowNext = document.querySelector('.slider__arrow_next');
-const dots = document.querySelectorAll('.slider__dot');
-const sliderItems = Array.from(document.querySelectorAll('.slider__item'));
-let currentSlideIndex = sliderItems.indexOf(document.querySelector('.slider__item_active'));
+const dots = Array.from(document.querySelectorAll('.slider__dot'));
+const sliderItems = document.querySelectorAll('.slider__item');
+let currentSlideIndex = Array.from(sliderItems).indexOf(document.querySelector('.slider__item_active'));
 
-function deactivateSlides() {
-  sliderItems.forEach(item => {
-    if (item.classList.contains('slider__item_active')) {
-      item.classList.remove('slider__item_active');
-    };
-  });  
+//reactivating current slide to activate current dot
+activateSlide(currentSlideIndex);
+
+function deactivateSlide(index) {
+  sliderItems[index].classList.remove('slider__item_active');
+  dots[index].classList.remove('slider__dot_active');
 }
+
+function activateSlide(index) {  
+  sliderItems[index].classList.add('slider__item_active');
+  dots[index].classList.add('slider__dot_active');
+}
+
 //prev navigation
 sliderArrowPrev.onclick = function () {
+  deactivateSlide(currentSlideIndex);
   currentSlideIndex -= 1;
   if (currentSlideIndex < 0) currentSlideIndex = sliderItems.length - 1;
-  deactivateSlides();
-  sliderItems[currentSlideIndex].classList.add('slider__item_active');
+  activateSlide(currentSlideIndex);
 };
+
 //next navigation
 sliderArrowNext.onclick = function () {
+  deactivateSlide(currentSlideIndex);
   currentSlideIndex += 1;
   if (currentSlideIndex > sliderItems.length - 1) currentSlideIndex = 0;
-  deactivateSlides();
-  sliderItems[currentSlideIndex].classList.add('slider__item_active');
+  activateSlide(currentSlideIndex);
 };
+
 //dots navigation
 dots.forEach((dot, dotIndex) => {
   dot.addEventListener('click', e => {
-    deactivateSlides();
-    sliderItems[dotIndex].classList.add('slider__item_active');
+    deactivateSlide(currentSlideIndex);
     currentSlideIndex = dotIndex;
+    activateSlide(currentSlideIndex);
   });
 });
