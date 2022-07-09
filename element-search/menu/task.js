40bb88ad86143e314bf03cf04c83dcc264372d78
laps78 <9254384@mail.ru>
2022-07-09 18:29:02 +0300

    Second task reklamations completed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,23 @@
-const menuLinks = document.querySelectorAll('a.menu__link');
-const subMenus = document.querySelectorAll('ul.menu_sub');
+const menuLinks = document.querySelectorAll('.menu__link');
+const subMenus = document.querySelectorAll('.menu_sub');
 
 menuLinks.forEach(item => {
   item.addEventListener('click', e => {
-    subMenus.forEach(subMenu => {
-      if (subMenu.classList.contains('menu_active')) {
-        subMenu.classList.remove('menu_active');
+    let thisSubMenu = e.target.parentElement.querySelector('.menu_sub');
+    console.log('Click!');
+    if (thisSubMenu.classList.contains('menu_active')) {
+      thisSubMenu.classList.remove('menu_active');
+    } else {
+      subMenus.forEach(subMenu => {
+        if (subMenu.classList.contains('menu_active')) {
+          
+          subMenu.classList.remove('menu_active');
+        };
+      });
+      if (item.parentElement.querySelector('.menu_sub')) {
+        e.preventDefault();
       };
-    });
-    if (item.parentElement.querySelector('ul')) {
-      e.preventDefault();
-    };    
-    item.parentElement.querySelector('ul').classList.toggle('menu_active');    
+      item.parentElement.querySelector('ul').classList.toggle('menu_active');
+    };
   });
 });
