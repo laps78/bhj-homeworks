ed1896b97ea1b8afc7ee08fa757b6bf9dc4e6bcb
laps78 <9254384@mail.ru>
2022-07-10 12:46:15 +0300

    second task bugs was fixed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,19 +3,18 @@ const subMenus = document.querySelectorAll('.menu_sub');
 
 menuLinks.forEach(item => {
   item.addEventListener('click', e => {
+    if (item.parentElement.querySelector('.menu_sub')) {
+      e.preventDefault();
+    };
     let thisSubMenu = e.target.parentElement.querySelector('.menu_sub');
     if (thisSubMenu.classList.contains('menu_active')) {
       thisSubMenu.classList.remove('menu_active');
     } else {
       subMenus.forEach(subMenu => {
         if (subMenu.classList.contains('menu_active')) {
-          
           subMenu.classList.remove('menu_active');
         };
       });
-      if (item.parentElement.querySelector('.menu_sub')) {
-        e.preventDefault();
-      };
       item.parentElement.querySelector('ul').classList.toggle('menu_active');
     };
   });
