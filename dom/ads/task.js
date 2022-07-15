8bf20060fe542d5409e178e2975ae679f049579c
laps78 <9254384@mail.ru>
2022-07-15 17:14:53 +0300

    DOM: ads task is completed in basic variant

---
+++ dom/ads/task.js
@@ -0,0 +1,26 @@
+class Banner {
+  constructor(container) {
+    this.container = container;
+    this.rotator = container.querySelector('.rotator');
+    this.cases = this.rotator.children;
+    this.currentCase = this.rotator.querySelector('.rotator__case_active');
+    this.currentCaseIndex = Array.from(this.cases).indexOf(this.currentCase);
+    
+    this.rotate = this.spanRotator.bind(this);
+    
+    this.interval = setInterval(this.rotate, 1000);
+  }
+
+  spanRotator() {
+    this.cases[this.currentCaseIndex].classList.remove('rotator__case_active');
+    this.currentCaseIndex++;
+    if (this.currentCaseIndex > this.cases.length - 1) {
+      this.currentCaseIndex = 0;
+    }
+    this.cases[this.currentCaseIndex].classList.add('rotator__case_active');    
+  }
+} 
+
+document.querySelectorAll('p').forEach(banner => new Banner(banner));
+
+
