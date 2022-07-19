d07b868ec006303cb21fd1585cf5f6d09117b2d1
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-07-19 09:27:31 +0300

    html-forms/chat: 1st milestone commit

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -4,24 +4,46 @@ const messages = document.querySelector('.chat-widget__messages');
 
 function activateChat() {
   chatWidget.classList.add(activeWidgetClass);
-  document.addEventListener('keydown', keyBoardEventHandler);
+  new Chatbox(chatWidget);
 }
 
 function deactivateChat() {
   chatWidget.classList.remove(activeWidgetClass);
-  document.removeEventListener('keydown', keyBoardEventHandler);
 }
 
-function keyBoardEventHandler() {
-  
+class Chatbox {
+  constructor(chatWidget) {
+    this.input = chatWidget.querySelector('#chat-widget__input');
+    this.messages = chatWidget.querySelector('chat-widget__messages');
+    this.timeout = setInterval(this.chatBotMessage, 30000);
+
+    this.inputEventHandler();
+  }
+
+  inputEventHandler() {
+    this.input.addEventListener('keyup', e => {
+      if (e.key === 'Enter') {
+        this.userMessage(this.input.value);
+       }
+    })
+  }
+
+  chatBotMessage() {
+    console.log('bot says hi for 30 seconds left');
+  }
+
+  userMessage(message) {
+    console.log('user says');
+    console.log(message);
+    //TODO check input.value!
+    clearInterval(this.timeout);
+  }
 }
 
 document.addEventListener('click', e => {
   if (e.target.closest('.chat-widget') === chatWidget) {
     activateChat();
-    
   } else {
     deactivateChat();
-    
   }
 });
