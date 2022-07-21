08cf4c8c2f60117d369228e6493b96d92095181d
laps78 <9254384@mail.ru>
2022-07-21 13:36:17 +0300

    html-forms/chat: 2st milestone commit:questions to teacher

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,6 +1,18 @@
+'ues strict';
+
 const activeWidgetClass = 'chat-widget_active';
 const chatWidget = document.querySelector('.chat-widget');
 const messages = document.querySelector('.chat-widget__messages');
+const botMessages = [
+  'Где ваша совесть?',
+  'Кто тут?',
+  'К сожалению, все операторы сейчас заняты. Не пишите нам больше!',
+  'Добрый день! До свидания!',
+  'Вы не купили ни одного товара доя того, чтобы так с нами разговаривать!',
+  'Мы ничего не будем вам продавать!'
+]
+
+let hours, minutes;
 
 function activateChat() {
   chatWidget.classList.add(activeWidgetClass);
@@ -13,30 +25,63 @@ function deactivateChat() {
 
 class Chatbox {
   constructor(chatWidget) {
+    this.messages = chatWidget.querySelector('.chat-widget__messages');
     this.input = chatWidget.querySelector('#chat-widget__input');
+    this.input.value = '';
     this.messages = chatWidget.querySelector('chat-widget__messages');
-    this.timeout = setInterval(this.chatBotMessage, 30000);
 
+    this.timeout = setInterval(this.chatBotMessage, 5000);
+
+    this.input.focus();
     this.inputEventHandler();
   }
 
   inputEventHandler() {
     this.input.addEventListener('keyup', e => {
-      if (e.key === 'Enter') {
-        this.userMessage(this.input.value);
-       }
+      if (this.input.value !== ''){
+        if (e.key === 'Enter') {
+          this.userMessage(this.input.value);
+          this.input.value = '';
+        }
+      } else {
+        console.log('Пустая строка!');
+      }
     })
   }
 
+  get timeStamp() {
+    hours = new Date().getHours();
+    if (hours < 10) hours = '0' + hours;
+    minutes = new Date().getMinutes()
+    if (minutes < 10) minutes = '0' + minutes;
+
+    return `${hours}:${minutes} `;
+  }
+
+  autoScroll() {
+    messages.scrollTop = messages.scrollHeight
+  }
+
   chatBotMessage() {
-    console.log('bot says hi for 30 seconds left');
+    messages.innerHTML += `
+    <div class="message">
+      <div class="message__time">${this.timeStamp}</div>
+      <div class="message__text">${botMessages[Math.floor(Math.random() * (botMessages.length - 1))]}</div>
+    </div>`;
+    this.autoScroll();
+    clearInterval(this.timeout);
   }
 
   userMessage(message) {
-    console.log('user says');
-    console.log(message);
-    //TODO check input.value!
+    console.log(`user says`);
+    messages.innerHTML += `
+      <div div class="message message_client">
+        <div class="message__time">${this.timeStamp}</div>
+        <div class="message__text">${message}</div>
+      </div>`;
+    this.autoScroll();
     clearInterval(this.timeout);
+    this.chatBotMessage();
   }
 }
 
