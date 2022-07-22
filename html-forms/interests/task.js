e82e0dfe05780443afb88d8ced293cfc730f1861
laps78 <9254384@mail.ru>
2022-07-22 15:01:52 +0300

    html-forms 3rd task completed, empty lines deleted

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -17,7 +17,6 @@ Array.from(checkboxes).forEach(checkbox => {
       if (target.closest('.interests_main') && target.closest('.interests_active')) {
         let targetParent = target.closest('.interests_active').previousElementSibling.querySelector('.interest__check');
         let targetSibling = Array.from(target.closest('.interests_active').querySelectorAll('.interest__check'));
-        
         if (targetSibling.some(item => item.checked === true)) {
           targetParent.checked = true;
         } else {
@@ -25,10 +24,7 @@ Array.from(checkboxes).forEach(checkbox => {
         }
       }
     }
-    
     modifyParent(event.target);
     modifyChilds(event.target);
-
-    
   };
 });
