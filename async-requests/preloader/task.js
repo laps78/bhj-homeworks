056a55c1ffba51ec41fa9c39e3d693f97cf1db29
laps78 <9254384@mail.ru>
2022-08-02 11:57:26 +0300

    async-requests/preloader: basic task is solved

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,7 +1,6 @@
 const loader = document.querySelector('#loader');
 const items = document.querySelector('#items');
 const requestUrl = 'https://netology-slow-rest.herokuapp.com';
-const currencyHTML = `<div class="item__code">USD</div><div class="item__value">32</div><div class="item__currency">руб.</div>`;
 
 function hideLoader() {
   if (loader.classList.contains('loader_active')) {
@@ -15,19 +14,44 @@ function showLoader() {
   }
 }
 
+function makeValuteList(valutes) {
+  while (items.firstChild) {
+    items.removeChild(items.firstChild);
+  };
+
+  for (valute in valutes) {
+    makeValute(valutes[valute]);
+  }
+}
+
+function makeValute(valute) {
+  const newEmptyDiv = document.createElement('div');
+  newEmptyDiv.classList.add('item');
+  const valuteDiv = items.appendChild(newEmptyDiv);
+  const currencyHTML = `
+    <div class="item__code">${valute.CharCode}</div>
+    <div class="item__value">${valute.Value}</div>
+    <div class="item__currency">руб.</div>
+    `; 
+  valuteDiv.insertAdjacentHTML('afterBegin', currencyHTML);
+}
+
 function makeRequest() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', requestUrl, true);
+  xhr.responseType = 'json';
   xhr.send();
-  xhr.onload = responseHandler(xhr);
+  xhr.addEventListener('readystatechange', () => {
+    responseHandler(xhr);
+  });
 }
 
 function responseHandler(xhr) {
   if (xhr.readyState === xhr.DONE) {
+    const valutes = xhr.response.response.Valute;
+    makeValuteList(valutes);
     hideLoader();
-    console.log(xhr.responseText);
   }
-  
 }
 
 function localStorageUpdater() {
