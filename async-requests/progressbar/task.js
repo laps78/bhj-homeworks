537a04da3ff3f1edaceafe269fa8de9c4f88edcb
laps78 <9254384@mail.ru>
2022-08-08 11:54:23 +0300

    async-requests/progressbar: task is solved

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,19 +4,13 @@ const form = document.forms.form;
 
 sendBtn.addEventListener('click', e => {
   e.preventDefault();
-  const formData = new FormData(form);
-  
-  console.log(form.file.files[0]);//file seems to be defined
-  
+  const formData = new FormData(form); 
   formData.append('file', form.file.files[0]);
-  
-  console.log(formData);//seems to be empty
-  
   const xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-  xhr.send(formData);
   xhr.upload.addEventListener('progress', function (progress) {
     console.log(progress.loaded);
     progressBar.value = progress.loaded / form.file.files[0].size;
   });
+  xhr.send(formData);
 });
